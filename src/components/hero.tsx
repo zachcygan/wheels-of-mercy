c3dbc0ed1179f84a8f80bdd0cef76b3ea17eccde
zachcygan <zachcygan@gmail.com>
2023-08-07 13:15:04 -0700

    increased size of carousel to adjust for image text and length

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -1,13 +1,54 @@
 'use client';
-import { useState } from "react";
+import { useState } from 'react'
+import { Dialog } from '@headlessui/react'
+import { Bars3Icon, XMarkIcon } from '@heroicons/react/24/outline'
+import Image from 'next/image'
 
+export default function Example() {
+  const [mobileMenuOpen, setMobileMenuOpen] = useState(false)
 
-export default function Hero() {
-
-
-    return (
-        <div className="max-w-4xl mx-auto text-black">
-            text
+  return (
+    <div className="bg-white pb-14">
+      <div className="relative isolate px-6 pt-14 lg:px-8">
+        <div className="mx-auto max-w-2xl">
+          <div className="hidden sm:mb-8 sm:flex justify-center">
+            {/* <div className="relative rounded-full px-3 py-1 text-sm leading-6 text-gray-600 ring-1 ring-gray-900/10 hover:ring-gray-900/20">
+              Announcing our next round of funding.{' '}
+              <a href="#" className="font-semibold text-indigo-600">
+                <span className="absolute inset-0" aria-hidden="true" />
+                Read more <span aria-hidden="true">&rarr;</span>
+              </a>
+            </div> */}
+            <Image
+                src='/assets/images/wheelsOfMercyLogo.png'
+                alt="Wheels of Mercy Logo"
+                width={700}
+                height={700}
+                className=''
+             />
+          </div>
+          <div className="text-center">
+            {/* <h1 className="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">
+              Data to enrich your online business
+            </h1> */}
+            <p className="mt-6 text-lg leading-8 text-gray-600">
+              Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui lorem cupidatat commodo. Elit sunt amet
+              fugiat veniam occaecat fugiat aliqua.
+            </p>
+            {/* <div className="mt-10 flex items-center justify-center gap-x-6">
+              <a
+                href="#"
+                className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+              >
+                Get started
+              </a>
+              <a href="#" className="text-sm font-semibold leading-6 text-gray-900">
+                Learn more <span aria-hidden="true">→</span>
+              </a>
+            </div> */}
+          </div>
         </div>
-    )
+      </div>
+    </div>
+  )
 }
