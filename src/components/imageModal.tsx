ca886cc31aaea0a37fd4ceefbaf9416f5e38a48d
zachcygan <zachcygan@gmail.com>
2023-08-25 20:57:20 -0700

    removed bg from modal

--- src/components/imageModal.tsx
+++ src/components/imageModal.tsx
@@ -47,7 +47,7 @@ export default function ImageModal({ isOpen, onClose, imgSrc }: ImageModalrProps
                             leaveFrom="opacity-100 translate-y-0 sm:scale-100"
                             leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
                         >
-                            <Dialog.Panel className="relative transform overflow-hidden rounded-lg bg-white px-4 pb-4 pt-5 text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg sm:p-6">
+                            <Dialog.Panel className="relative transform overflow-hidden rounded-lg px-4 pb-4 pt-5 text-left transition-all sm:my-8 sm:w-full sm:max-w-lg sm:p-6">
                                 <div className="absolute right-0 top-0 hidden pr-4 pt-4 sm:block">
                                     <button
                                         type="button"
