5ad941070d4911d463abfbb3c1ba4cbc9fe91c80
zachcygan <zachcygan@gmail.com>
2023-08-30 18:02:09 -0700

    removed metadata tag

--- src/app/faq/page.tsx
+++ src/app/faq/page.tsx
@@ -1,11 +1,5 @@
 'use client'
 import FrequentlyAskedQuestions from '../../components/frequentlyAskedQuestions'
-import { Metadata } from 'next'
-
-export const metadata: Metadata = {
-    title: 'Wheels of Mercy - Frequently Asked Questions',
-    description: 'Frequently Asked Questions about Wheels of Mercy'
-}
 
 export default function Faq() {
     return (
