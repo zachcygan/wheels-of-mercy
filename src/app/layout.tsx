eb12e94cf899af2233393300102636566e1602ad
zachcygan <zachcygan@gmail.com>
2023-09-09 21:43:51 -0700

    added analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import './globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
+import { Analytics } from '@vercel/analytics/react';
 import Navbar from "../components/navbar"
 import Footer from "../components/footer"
 import Head from 'next/head'
@@ -26,6 +27,7 @@ export default function RootLayout({
         </header>
         <div className='min-h-[73vh]'>
           {children}
+          <Analytics />
         </div>
         <Footer />
 
