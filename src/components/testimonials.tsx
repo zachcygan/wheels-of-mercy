604679613534fdf5f545a7b5b99e5bdc47c098cb
zachcygan <zachcygan@gmail.com>
2023-09-14 20:22:48 -0700

    changed countless to many

--- src/components/testimonials.tsx
+++ src/components/testimonials.tsx
@@ -72,7 +72,7 @@ export default function Testimonials() {
             <div className="mx-auto max-w-7xl px-6 lg:px-8">
                 <div className="mx-auto max-w-xl text-center">
                     <p className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
-                        We have helped countless amazing people
+                        We have helped many amazing people
                     </p>
                 </div>
                 <div className="mx-auto mt-16 grid max-w-2xl grid-cols-1 grid-rows-1 gap-8 text-sm leading-6 text-gray-900 sm:mt-20 sm:grid-cols-2 xl:mx-0 xl:max-w-none xl:grid-flow-col xl:grid-cols-4">
