ab796ec97833000bc48593992b3cd199248a2068
zachcygan <zachcygan@gmail.com>
2023-09-15 20:08:20 -0700

    save

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -32,7 +32,7 @@ export default function Navbar() {
       {({ open }) => (
         <>
           <div className="bg-white/70 mx-auto px-2 sm:px-6 lg:px-8 -z-10">
-            <div className="relative flex h-16 items-center">
+            <div className="relative flex h-16 items-center justify-between w-full">
               <div className="absolute inset-y-0 right-0 flex items-center min-[900px]:hidden">
                 {/* Mobile menu button*/}
                 <Disclosure.Button className="inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
@@ -44,71 +44,73 @@ export default function Navbar() {
                   )}
                 </Disclosure.Button>
               </div>
-              <div className='flex justify-between items-center'>
-                <div className="text-black">
-                  <Link
-                    key={'Wheels of Mercy'}
-                    href={'/'}
-                    className='flex items-center justify-center'
-                  >
-                    <Image
-                      src="/assets/images/wheelsOfMercy.png"
-                      alt="Wheels of Mercy"
-                      width={180}
-                      height={50}
-                      priority
-                    />
-                  </Link>
-                </div>
-                <div className="flex-1">
-                  <div className="hidden sm:ml-6 sm:block">
-                    <div className="flex items-center justify-center max-[900px]:hidden">
-                      {navigation.map((item) => {
-                        const isActive = item.href === pathname;
-                        return (
-                          <Link
-                            key={item.name}
-                            href={item.href}
-                            className={`px-2 py-2 rounded-md text-lg relative no-underline duration-200 ease-in hover:text-white ${isActive ? "text-white" : "text-black"
-                              }`}
-                            aria-current={item.href === pathname ? 'page' : undefined}
-                            onClick={(e) => {
-                              if (item.name === 'Contact') {
-                                e.preventDefault(); // prevent navigation
-                                setIsSlideOpen(true);
-                              }
-                            }}
-                            onMouseOver={() => setHoveredPath(item.href)}
-                            onMouseLeave={() => setHoveredPath(pathname)}
-                          >
-                            <span>{item.name}</span>
-                            {item.href === hoveredPath && (
-                              <motion.div
-                                className="absolute bottom-0 left-0 h-full bg-gray-900 rounded-md -z-10"
-                                layoutId="navbar"
-                                aria-hidden="true"
-                                style={{
-                                  width: "100%",
-                                }}
-                                transition={{
-                                  type: "spring",
-                                  bounce: 0.25,
-                                  stiffness: 130,
-                                  damping: 9,
-                                  duration: 0.2,
-                                }}
-                              />
-                            )}
-                          </Link>
-                        );
-                      })}
-                    </div>
-                    <SlideOver isOpen={isSlideOpen} onClose={() => setIsSlideOpen(false)} />
+              <div className="text-black">
+                <Link
+                  key={'Wheels of Mercy'}
+                  href={'/'}
+                  className='flex items-center justify-center'
+                >
+                  <Image
+                    src="/assets/images/wheelsOfMercy.png"
+                    alt="Wheels of Mercy"
+                    width={180}
+                    height={50}
+                    priority
+                  />
+                </Link>
+              </div>
+              <div className="flex-1">
+                <div className="hidden sm:ml-6 sm:block">
+                  <div className="flex items-center justify-center max-[900px]:hidden">
+                    {navigation.map((item) => {
+                      const isActive = item.href === pathname;
+                      return (
+                        <Link
+                          key={item.name}
+                          href={item.href}
+                          className={`px-2 py-2 rounded-md text-lg relative no-underline duration-200 ease-in hover:text-white ${isActive ? "text-white" : "text-black"
+                            }`}
+                          aria-current={item.href === pathname ? 'page' : undefined}
+                          onClick={(e) => {
+                            if (item.name === 'Contact') {
+                              e.preventDefault(); // prevent navigation
+                              setIsSlideOpen(true);
+                            }
+                          }}
+                          onMouseOver={() => setHoveredPath(item.href)}
+                          onMouseLeave={() => setHoveredPath(pathname)}
+                        >
+                          <span>{item.name}</span>
+                          {item.href === hoveredPath && (
+                            <motion.div
+                              className="absolute bottom-0 left-0 h-full bg-gray-900 rounded-md -z-10"
+                              layoutId="navbar"
+                              aria-hidden="true"
+                              style={{
+                                width: "100%",
+                              }}
+                              transition={{
+                                type: "spring",
+                                bounce: 0.25,
+                                stiffness: 130,
+                                damping: 9,
+                                duration: 0.2,
+                              }}
+                            />
+                          )}
+                        </Link>
+                      );
+                    })}
                   </div>
+                  <SlideOver isOpen={isSlideOpen} onClose={() => setIsSlideOpen(false)} />
                 </div>
-                {/* <div>
-                  test
-                </div> */}
+              </div>
+              <div>
+                <button 
+                  className="rounded-md bg-gray-400 p-2 hover:text-white hover:scale-110 transition-all active:scale-105"
+                >
+                  Join Our Mailing List
+                </button>
               </div>
             </div>
           </div>
