1644251991ba7ecc9a55053f56ed1492be278a12
zachcygan <zachcygan@gmail.com>
2023-09-19 13:25:18 -0700

    added link to donate page on support page

--- src/components/supportPage.tsx
+++ src/components/supportPage.tsx
@@ -1,15 +1,19 @@
 'use client'
+import Link from 'next/link'
 
 export default function SupportPage() {
     return (
         <div>
-            <div className="px-6 py-24 sm:py-32 lg:px-8">
+            <div className="px-6py-14 sm:py-20">
                 <div className="mx-auto max-w-2xl text-center">
                     <h2 className="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">Get Involved</h2>
                     <p className="mt-6 text-lg leading-8 text-gray-600">
                         Financial resources are the lifeblood of a non-profit. Wheels of Mercy is an entirely volunteer organization.
                         Your financial support will pay for things like fuel for volunteers who pick up wheelchairs, spare parts, and
-                        shipping costs. So give if you can. LINK TO DONATION PAGE/DONATE BUTTON
+                        shipping costs. So give if you can. 
+                        <Link href='/donate' className='pl-1'>
+                            <span className='underline'>Donate here</span>
+                        </Link>
                     </p>
                     <p className="mt-8 text-lg leading-8 text-gray-600">
                         If you possess a particular skill or knowledge base that can further our mission, we want to hear from you. If
