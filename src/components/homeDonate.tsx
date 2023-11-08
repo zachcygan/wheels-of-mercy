0820276530d3581ca9c1badad28b6e112e393378
zachcygan <zachcygan@gmail.com>
2023-11-07 16:03:25 -0800

    added padding

--- src/components/homeDonate.tsx
+++ src/components/homeDonate.tsx
@@ -4,30 +4,28 @@ import DonationButton from "./donationButton"
 
 export default function HomeDonate() {
   return (
-    <div className="flex justify-center items-center"> {/* This will center the container horizontally on the page */}
-      <div className="overflow-hidden rounded-lg bg-gray-50 max-w-7xl dark:bg-gray-600/20 flex-1">
-          <div className="flex flex-col md:flex-row"> {/* Removed h-full */}
-            <div className="flex-1 flex flex-col justify-center items-center"> {/* This makes the div a flex container and centers its children vertically and horizontally */}
-              <div>EVERY gift counts</div>
-              <div>EVERY gift makes a difference</div>
-              <div>EVERY gift is accepted with gratitude</div>
-              <div>GIVE TODAY!</div>
-              <div
-                className="py-5"
-              >
-                <DonationButton />
-              </div>
-            </div>
-            <div className="flex-1 flex justify-center items-center">
-              <Image
-                src={'/assets/images/homeDonate.jpg'}
-                alt="Donate Image"
-                layout="responsive"
-                width={500}
-                height={500}
-              />
+    <div className="flex justify-center items-center w-full px-5"> {/* Ensure full width and centering */}
+      <div className="overflow-hidden rounded-lg bg-gray-50 max-w-7xl dark:bg-gray-600/20 w-full">
+        <div className="flex flex-col md:flex-row"> {/* Ensuring the flex container takes up the full width */}
+          <div className="flex-1 flex flex-col justify-center items-center text-center py-2 w-full"> {/* Added text-center and w-full */}
+            <div className="text-2xl md:text-4xl">EVERY gift counts</div>
+            <div className="text-2xl md:text-4xl">EVERY gift makes a difference</div>
+            <div className="text-2xl md:text-4xl">EVERY gift is accepted with gratitude</div>
+            <div className="text-2xl md:text-4xl">GIVE TODAY!</div>
+            <div className="py-5">
+              <DonationButton />
             </div>
           </div>
+          <div className="flex-1 flex justify-center items-center">
+            <Image
+              src={'/assets/images/homeDonate.jpg'}
+              alt="Donate Image"
+              layout="responsive"
+              width={500}
+              height={500}
+            />
+          </div>
+        </div>
       </div>
     </div>
   )
--- src/components/testimonials.tsx
+++ src/components/testimonials.tsx
@@ -2,105 +2,105 @@ import { Roboto } from 'next/font/google'
 import { useEffect } from 'react'
 
 const robotoFont = Roboto({
-    subsets: ['latin'],
-    weight: '700'
+  subsets: ['latin'],
+  weight: '700'
 })
 
 const testimonials = [
-    {
-        body: 'This is such a blessing. Thank you, Thank you, Thank you!',
-        author: {
-            name: 'Maria Rodriguez',
-            imageUrl:
-                '/assets/images/testimonial1.jpg',
-        },
+  {
+    body: 'This is such a blessing. Thank you, Thank you, Thank you!',
+    author: {
+      name: 'Maria Rodriguez',
+      imageUrl:
+        '/assets/images/testimonial1.jpg',
     },
-    {
-        body: 'This is a miracle! God bless you!',
-        author: {
-            name: 'Avelino Rodriguez',
-            imageUrl:
-                '/assets/images/testimonial2.webp',
-        },
+  },
+  {
+    body: 'This is a miracle! God bless you!',
+    author: {
+      name: 'Avelino Rodriguez',
+      imageUrl:
+        '/assets/images/testimonial2.webp',
     },
-    {
-        body: 'This is the best thing that’s ever happened to me!',
-        author: {
-            name: 'Victor Vanisacker',
-            imageUrl:
-                '/assets/images/carouselImage6.webp',
-        },
+  },
+  {
+    body: 'This is the best thing that’s ever happened to me!',
+    author: {
+      name: 'Victor Vanisacker',
+      imageUrl:
+        '/assets/images/carouselImage6.webp',
     },
-    {
-        body: 'We are so grateful to Charles! Vernon loves his new Chair! Without Charles’ help Vernon would have never been approved for his new chair.',
-        author: {
-            name: 'Kaye Doan',
-            imageUrl:
-                '/assets/images/testimonial1.webp',
-        },
+  },
+  {
+    body: 'We are so grateful to Charles! Vernon loves his new Chair! Without Charles’ help Vernon would have never been approved for his new chair.',
+    author: {
+      name: 'Kaye Doan',
+      imageUrl:
+        '/assets/images/testimonial1.webp',
     },
-    {
-        body: 'We are so grateful to Charles! Vernon loves his new Chair! Without Charles’ help Vernon would have never been approved for his new chair.',
-        author: {
-            name: 'PLACEHOLDER',
-            imageUrl:
-                '/assets/images/testimonial1.webp',
-        },
+  },
+  {
+    body: 'We are so grateful to Charles! Vernon loves his new Chair! Without Charles’ help Vernon would have never been approved for his new chair.',
+    author: {
+      name: 'PLACEHOLDER',
+      imageUrl:
+        '/assets/images/testimonial1.webp',
     },
-    // More testimonials...
+  },
+  // More testimonials...
 ]
 
 export default function Testimonials() {
-    useEffect(() => {
-        const observer = new IntersectionObserver((entries) => {
-            entries.forEach((entry) => {
-                if (entry.isIntersecting) {
-                    entry.target.classList.add('show')
-                }
-            })
-        });
-        const hiddenElements = document.querySelectorAll('.hiddenClass');
-        hiddenElements.forEach((element) => {
-            observer.observe(element);
-        });
+  useEffect(() => {
+    const observer = new IntersectionObserver((entries) => {
+      entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+          entry.target.classList.add('show')
+        }
+      })
+    });
+    const hiddenElements = document.querySelectorAll('.hiddenClass');
+    hiddenElements.forEach((element) => {
+      observer.observe(element);
+    });
 
-        // Cleanup observer on component unmount
-        return () => {
-            hiddenElements.forEach((element) => {
-                observer.unobserve(element);
-            });
-        };
-    }, []);
+    // Cleanup observer on component unmount
+    return () => {
+      hiddenElements.forEach((element) => {
+        observer.unobserve(element);
+      });
+    };
+  }, []);
 
-    return (
-        <div className="py-4 sm:py-10">
-            <div className="mx-auto max-w-7xl px-6 lg:px-8">
-                <div className="mx-auto max-w-2xl pb-10 text-center">
-                    {/* <h2 className="text-lg font-semibold leading-8 tracking-tight text-indigo-600">Testimonials</h2> */}
-                    <p className={`hiddenClass mt-2 text-4xl font-bold tracking-tight text-black dark:text-dark sm:text-6xl ${robotoFont.className}`}>
-                        We Have Helped Many Amazing People
-                    </p>
-                </div>
-                <div className="mx-auto mt-16 flow-root max-w-2xl sm:mt-20 lg:mx-0 lg:max-w-none">
-                    <div className="-mt-8 sm:-mx-4 sm:columns-2 sm:text-[0] lg:columns-3 2xl:scale-125">
-                        {testimonials.map((testimonial) => (
-                            <div key={testimonial.author.name} className="hiddenClass test pt-8 sm:inline-block sm:w-full sm:px-4">
-                                <figure className="rounded-2xl bg-gray-50 p-8 text-sm leading-6">
-                                    <blockquote className="text-gray-900">
-                                        <p>{`“${testimonial.body}”`}</p>
-                                    </blockquote>
-                                    <figcaption className="mt-6 flex items-center gap-x-4">
-                                        <img className="h-10 w-10 rounded-full bg-gray-50" src={testimonial.author.imageUrl} alt="" />
-                                        <div>
-                                            <div className="font-semibold text-gray-900">{testimonial.author.name}</div>
-                                        </div>
-                                    </figcaption>
-                                </figure>
-                            </div>
-                        ))}
-                    </div>
-                </div>
-            </div>
+  return (
+    <div className="py-4 sm:py-10">
+      <div className="mx-auto max-w-7xl px-6 lg:px-8">
+        <div className="mx-auto max-w-2xl pb-10 text-center">
+          {/* <h2 className="text-lg font-semibold leading-8 tracking-tight text-indigo-600">Testimonials</h2> */}
+          <p className={`hiddenClass mt-2 text-4xl font-bold tracking-tight text-black dark:text-dark sm:text-6xl ${robotoFont.className}`}>
+            We Have Helped Many Amazing People
+          </p>
         </div>
-    )
+        <div className="mx-auto mt-16 flow-root max-w-2xl sm:mt-20 lg:mx-0 lg:max-w-none">
+          <div className="-mt-8 sm:-mx-4 sm:columns-2 sm:text-[0] lg:columns-3 2xl:scale-125">
+            {testimonials.map((testimonial) => (
+              <div key={testimonial.author.name} className="hiddenClass test pt-8 sm:inline-block sm:w-full sm:px-4">
+                <figure className="rounded-2xl bg-gray-50 p-8 text-sm leading-6">
+                  <blockquote className="text-gray-900">
+                    <p>{`“${testimonial.body}”`}</p>
+                  </blockquote>
+                  <figcaption className="mt-6 flex items-center gap-x-4">
+                    <img className="h-10 w-10 rounded-full bg-gray-50" src={testimonial.author.imageUrl} alt="" />
+                    <div>
+                      <div className="font-semibold text-gray-900">{testimonial.author.name}</div>
+                    </div>
+                  </figcaption>
+                </figure>
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
+    </div>
+  )
 }
