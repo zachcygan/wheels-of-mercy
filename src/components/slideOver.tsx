6b8eda1bbb507d0ee5bfbe6af91cbcc12f407f4f
Zach Cygan <zachcygan@gmail.com>
2024-05-04 20:58:22 -0700

    changed text color

--- src/components/slideOver.tsx
+++ src/components/slideOver.tsx
@@ -53,7 +53,7 @@ export default function SlideOver({ isOpen, onClose }: SlideOverProps) {
                     <div className="flex h-full flex-col overflow-y-scroll bg-white py-6 shadow-xl dark:bg-gray-700">
                       <div className="px-4 sm:px-6">
                         <div className="flex items-start justify-between">
-                          <Dialog.Title className="text-base font-semibold leading-6 text-gray-900">
+                          <Dialog.Title className="text-base font-semibold leading-6 text-gray-900 dark:text-dark">
                             Contact Form
                           </Dialog.Title>
                           <div className="ml-3 flex h-7 items-center">
