54240b178f56aa74ac7d18fa8fd7f0cd07a99b94
Zach Cygan <zachcygan@gmail.com>
2024-05-04 22:02:11 -0700

    added select component

--- src/components/contactForm.tsx
+++ src/components/contactForm.tsx
@@ -2,6 +2,8 @@
 import { useState, useEffect, useRef } from 'react'
 import { PhotoIcon } from '@heroicons/react/24/solid'
 import { useFormData } from './formContext'
+import { Field, Label } from '@/components/fieldset'
+import { Select } from './select'
 import emailjs from '@emailjs/browser'
 import Success from './success'
 import Error from './error'
@@ -175,25 +177,6 @@ export default function ContactForm() {
                     </label>{' '}
                   </div>
                 </div>
-                {/* <div className="relative flex items-start">
-                  <div className="flex h-6 items-center">
-                    <input
-                      id="donateInKind"
-                      aria-describedby="candidates-description"
-                      name="checkboxes"
-                      type="checkbox"
-                      className="outline-none h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-600"
-                      value='Donate in Kind'
-                      onChange={(e) => handleCheckboxChange(e, 'Donate in Kind')}
-                      checked={checkboxes.includes('DonateInKind')}
-                    />
-                  </div>
-                  <div className="ml-3 text-sm leading-6">
-                    <label htmlFor="candidates" className="font-medium text-gray-900 dark:text-dark">
-                      Donate in Kind
-                    </label>{' '}
-                  </div>
-                </div> */}
                 <div className="relative flex items-start">
                   <div className="flex h-6 items-center">
                     <input
@@ -259,7 +242,6 @@ export default function ContactForm() {
                   </div>
                 </div>
               </div>
-              {/* State select */}
               <div className="sm:col-span-3">
                 <label htmlFor="state" className="block text-sm font-medium leading-6 text-gray-900 dark:text-dark">
                   State
@@ -271,7 +253,7 @@ export default function ContactForm() {
                     autoComplete="state"
                     value={formData.state}
                     onChange={handleSelectChange}
-                    className="rounded-md ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 block flex-1 border-0 bg-transparent py-1.5 pl-1 text-gray-900 dark:text-dark placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6"
+                    className="w-full appearance-none rounded-md ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 block flex-1 border-0 bg-transparent py-1.5 pl-1 text-gray-900 dark:text-dark placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6"
                   >
                     <option value="" className='text-black'>Select a state</option>
                     {states.map((state, index) => (
